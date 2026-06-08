ab41f85b02fbcb8f871c3a7ed5eab5fd6b41f55f
Md Nahid Hassan Bhuiyan <nahid151341@gmail.com>
2026-06-08 18:12:17 +0600

    installed the basic projects and commander is working

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import { Command } from "commander";
+
+
+const program = new Command();
+
+program.name("TwoClaw").description("TwoClaw is a CLI tool for running two-command workflows.").action(
+    async () => {
+        console.log("TwoClaw is running...");
+    }
+)
+
+
+
+program.parse(process.argv);
